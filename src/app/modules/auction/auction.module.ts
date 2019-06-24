04842389917dbc1919618218f5e3e49472cb82e3
Sullivan Brooks <sbrooks@cogencyglobal.com>
2019-06-24 16:58:08 -0500

    Auction Graph Stuff

--- src/app/modules/auction/auction.module.ts
+++ src/app/modules/auction/auction.module.ts
@@ -4,12 +4,14 @@ import { AuctionComponent } from './auction.component';
 import { AuctionDetailsComponent } from './components/auction-details/auction-details.component';
 import { AuctionGraphComponent } from './components/auction-graph/auction-graph.component';
 import { ChartsModule } from 'ng2-charts';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuctionComponent, AuctionDetailsComponent, AuctionGraphComponent],
   imports: [
     CommonModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule
   ]
 })
 export class AuctionModule { 
