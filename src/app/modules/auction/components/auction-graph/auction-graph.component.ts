a48109ab49e667174550aa20e63ba2cae5884463
Sully Brooks <sullybrooks@icloud.com>
2019-07-09 13:38:43 -0500

    Tooltips added to bids, strike line removed -temp

--- src/app/modules/auction/components/auction-graph/auction-graph.component.ts
+++ src/app/modules/auction/components/auction-graph/auction-graph.component.ts
@@ -40,7 +40,7 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
   ngAfterViewInit() {
     console.log("View Initialized");
     if (this.auction && this.bids) {
-      console.log("Building Chart");
+      console.log("Building Chart"); 
       this.buildChart();
     }
   }
@@ -50,49 +50,63 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
 
   
     // Scale the range of the data again
-    this.chartProps.x.domain(d3.extent(this.bids, function (d) {
+    // this.chartProps.x.domain(d3.extent(this.bids, function (d) {
       
-        return d.pps;
+    //     return d.pps;
       
-    }));
+    // }));
+
+    var xExtent = d3.extent(this.bids, function(b) { return b.pps; }),
+  xRange = xExtent[1] - xExtent[0],
+  yExtent = d3.extent(this.bids, function(b) { return b.numShares; }),
+  yRange = yExtent[1] - yExtent[0];
+
+// set domain to be extent +- 5%
+this.chartProps.x.domain([xExtent[0] - (xRange * .05), xExtent[1] + (xRange * .05)]);
+this.chartProps.y.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);
   
-    this.chartProps.y.domain(d3.extent(this.bids, function(d){
-      return d.numShares;
-    }));
+    // this.chartProps.y.domain(d3.extent(this.bids, function(d){
+    //   return d.numShares;
+    // }));
     // this.chartProps.y.domain([0, d3.max(this.bids, function (d) { return Math.max(d.pps, d.numShares); })]);
   
     // Select the section we want to apply our changes to
     this.chartProps.svg.transition();
   
     // Make the changes to the line chart
-    this.chartProps.svg.select('.line.strikePriceLine') // update the line
-      .attr('d', this.chartProps.strikePriceLine(this.bids));
+    // this.chartProps.svg.select('.line.strikePriceLine') // update the line
+    //   .attr('d', this.chartProps.strikePriceLine(this.bids));
   
-    // this.chartProps.svg.select('.line.line2') // update the line
-    //   .attr('d', this.chartProps.valueline2(this.bids));
-  
+    var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
     this.chartProps.svg.select('.x.axis') // update x axis
       .call(this.chartProps.xAxis);
   
     this.chartProps.svg.select('.y.axis') // update y axis
       .call(this.chartProps.yAxis);
 
-      this.chartProps.svg.append("g")
-      .attr("stroke-width", 1.5)
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-    .selectAll("g")
-    .data(this.bids)
-  .join("g")
-    .attr("transform", d => `translate(${this.chartProps.x(d.pps)},${this.chartProps.y(d.numShares)})`)
-    .call(g => g.append("circle")
-        .attr("stroke", "steelblue")
-        .attr("fill", "none")
-        .attr("r", 3))
-    .call(g => g.append("text")
-        .attr("dy", "0.35em")
-        .attr("x", 7)
-        .text(d => "PPS: " + d.pps + " # Shares: " + d.numShares));
+      this.chartProps.svg.selectAll("dot")
+      .data(this.bids)
+    .enter().append("circle")
+      .attr("r", 5)
+      .attr("cx", function(d) { return _this.chartProps.x(d.pps); })
+      .attr("cy", function(d) { return _this.chartProps.y(d.numShares); })
+      .on("mouseover", function(d) {
+        div.transition()
+          .duration(200)
+          .style("opacity", .9)
+          .style("width", "auto")
+          .style("height", "auto");
+        div.html("PPS: $" + d.pps + "<br/> # Shares: " + d.numShares)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+        })
+      .on("mouseout", function(d) {
+        div.transition()
+          .duration(500)
+          .style("opacity", 0);
+        });
   }
 
 
@@ -120,14 +134,9 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       })
       .y(function (d) { return _this.chartProps.y(d.numShares); });
 
-    // // Define the line
-    // var valueline2 = d3.line<Bid>()
-    //   .x(function (d) {
-
-    //       return _this.chartProps.x(d.pps);
-
-    //   })
-    //   .y(function (d) { console.log('Open market'); return _this.chartProps.y(d.open); });
+      var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
     var svg = d3.select(this.chartElement.nativeElement)
       .append('svg')
@@ -136,49 +145,23 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    // Scale the range of the data
-    this.chartProps.x.domain(
-      d3.extent(_this.bids, function (d) {
-        return d.pps;
-      }));
 
-      this.chartProps.y.domain(d3.extent(this.bids, function(d){
-        return d.numShares;
-      }));
-    // this.chartProps.y.domain([0, d3.max(this.bids, function (d) {
-    //   return Math.max(d.pps, d.numShares);
-    // })]);
-
-    // // Add the valueline2 path.
-    // svg.append('path')
-    //   .attr('class', 'line line2')
-    //   .style('stroke', 'green')
-    //   .style('fill', 'none');
-    //   // .attr('d', valueline2(_this.bids));
+      var xExtent = d3.extent(this.bids, function(b) { return b.pps; }),
+      xRange = xExtent[1] - xExtent[0],
+      yExtent = d3.extent(this.bids, function(b) { return b.numShares; }),
+      yRange = yExtent[1] - yExtent[0];
+    
+    // set domain to be extent +- 5%
+    this.chartProps.x.domain([xExtent[0] - (xRange * .05), xExtent[1] + (xRange * .05)]);
+    this.chartProps.y.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);
 
     // Add the valueline path.
-    svg.append('path')
-      .attr('class', 'line strikePriceLine')
-      .style('stroke', 'black')
-      .style('fill', 'none')
-      .attr('d', strikePriceLine(_this.bids));
+    // svg.append('path')
+    //   .attr('class', 'line strikePriceLine')
+    //   .style('stroke', 'black')
+    //   .style('fill', 'none')
+    //   .attr('d', strikePriceLine(_this.bids));
 
-    svg.append("g")
-      .attr("stroke-width", 1.5)
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-    .selectAll("g")
-    .data(this.bids)
-  .join("g")
-    .attr("transform", d => `translate(${this.chartProps.x(d.pps)},${this.chartProps.y(d.numShares)})`)
-    .call(g => g.append("circle")
-        .attr("stroke", "steelblue")
-        .attr("fill", "none")
-        .attr("r", 3))
-    .call(g => g.append("text")
-        .attr("dy", "0.35em")
-        .attr("x", 7)
-        .text(d => "PPS: " + d.pps + " # Shares: " + d.numShares));
 
     // Add the X Axis
     svg.append('g')
@@ -191,6 +174,27 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       .attr('class', 'y axis')
       .call(yAxis);
 
+      svg.selectAll("dot")
+     .data(this.bids)
+   .enter().append("circle")
+     .attr("r", 5)
+     .attr("cx", function(d) { return _this.chartProps.x(d.pps); })
+     .attr("cy", function(d) { return _this.chartProps.y(d.numShares); })
+     .on("mouseover", function(d) {
+       div.transition()
+         .duration(200)
+         .style("opacity", .9)
+         .style("width", "auto")
+         .style("height", "auto");
+       div.html("PPS: $" + d.pps + "<br/> # Shares: " + d.numShares)
+         .style("left", (d3.event.pageX) + "px")
+         .style("top", (d3.event.pageY - 28) + "px");
+       })
+     .on("mouseout", function(d) {
+       div.transition()
+         .duration(500)
+         .style("opacity", 0);
+       });
 
 
     // Setting the required objects in chartProps so they could be used to update the chart
