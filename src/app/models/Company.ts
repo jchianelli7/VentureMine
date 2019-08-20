4abd0e8d00deb97ac0d730e15c9de3994ec030e6
Sully Brooks <sullybrooks@icloud.com>
2019-08-20 13:57:00 -0500

    Admin Panel Start

--- src/app/models/Company.ts
+++ src/app/models/Company.ts
@@ -1,8 +1,13 @@
 import { Auction } from './Auction';
 import { User } from './User';
+import { Person } from './Person';
 
 export class Company {
     companyName: string;
     auctions: [Auction];
-    companyOwner: User;
+    generalBio: string;
+    images: [string];
+    staff: [Person];
+    documents: [string];        // Make sure this is the best way to do it
+    owner: User;
 }
