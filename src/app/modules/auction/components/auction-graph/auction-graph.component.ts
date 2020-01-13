2f5d1fa5c4e9d724db2687dbdd50de45854055f3
Josh Chianelli <joshchianelli@gmail.com>
2020-01-13 15:50:46 -0600

    [FEATURE] added stacked bar chart

--- src/app/modules/auction/components/auction-graph/auction-graph.component.ts
+++ src/app/modules/auction/components/auction-graph/auction-graph.component.ts
@@ -1,20 +1,18 @@
 import {
+  AfterViewInit,
   Component,
-  ViewEncapsulation,
-  OnInit,
-  OnChanges,
-  SimpleChange,
-  Input,
-  ViewChild,
   ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
   SimpleChanges,
-  AfterViewInit
+  ViewChild,
+  ViewEncapsulation
 } from '@angular/core';
 import * as d3 from 'd3';
 import {Auction} from 'src/app/models/Auction';
 import {Bid} from 'src/app/models/Bid';
 import {AuctionService} from 'src/app/services/auction-service.service';
-import {style} from 'd3';
 
 @Component({
   selector: 'app-auction-graph',
@@ -69,9 +67,13 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
     this.chartProps.y = d3.scaleLinear().range([height, 0]);
     this.chartProps.y2 = d3.scaleLinear().range([height, 0]);        // TODO: Manipulate this to look good AND COLOR CODE AXIS & DATA POINTS/LINES TO MATCH
 
-    var xExtent = d3.extent(this.auction.volumeData, function(d) { return d.pps; }),
+    var xExtent = d3.extent(this.auction.volumeData, function (d) {
+        return d.pps;
+      }),
       xRange = xExtent[1] - xExtent[0],
-      yExtent = d3.extent(this.auction.volumeData, function(d) { return d.shareCount; }),
+      yExtent = d3.extent(this.auction.volumeData, function (d) {
+        return d.shareCount;
+      }),
       yRange = yExtent[1] - yExtent[0];
 
     // Define the axes
@@ -87,7 +89,7 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       .attr('class', 'tooltip')
       .style('opacity', 0);
 
-    const svg = d3.select(this.chartElement.nativeElement)
+    var svg = d3.select(this.chartElement.nativeElement)
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('svg')
@@ -104,18 +106,20 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
     this.chartProps.y.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);
     this.chartProps.y2.domain([yExtent[0] - (yRange * .05), yExtent[1] + (yRange * .05)]);
 
-    const volumeArea = d3.area()
-      .x(function(b) {
-        return _this.chartProps.x(Number(b.pps));
-      })
-      .y0(height)
-      .y1(function(b) {
-        return _this.chartProps.y2(Number(b.shareCount));
-      })
-      .curve(d3.curveMonotoneX);
+    var data = this.parseVolumeData()
 
+    // List of subgroups = header of the csv files = soil condition here
+    var subgroups = data.columns.slice(1)
+    // List of groups = species here = value of the first column called group -> I show them on the X axis
+    var groups = d3.map(data, function (d) {
+      return (d.pps)
+    }).keys()
 
     // Add the X Axis
+    var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
     svg.append('g')
       .attr('class', 'x axis')
       .attr('transform', `translate(0,${height})`)
@@ -124,6 +128,9 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       .call(xAxis);
 
     // Add the y2 Axis
+    var y = d3.scaleLinear()
+      .domain([0, 300])
+      .range([height, 0]);
     svg.append('g')
       .attr('transform', 'translate(' + width + ' ,0)')
       .attr('class', 'y2 axis')
@@ -155,30 +162,47 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
       .style('fill', 'black')
       .text('Volume');
 
-    svg.selectAll('.bar')
-      .data(this.auction.volumeData)
-      .enter().append('rect')
-      .attr('class', 'bar')
-      .attr('fill', function(d) {
-        console.log(d);
-        return ((d.pps >= me.auction.currentStrikePrice) && (me.auction.reserveMet)) ? '#3FBF48' : 'steelblue';
+    // color palette = one color per subgroup
+    var color = d3.scaleOrdinal()
+      .domain(subgroups)
+      .range(['#4daf4a', '#e41a1c'])
+
+    //stack per subgroup
+    var stackedData = d3.stack()
+      .keys(subgroups)
+      (data)
+
+    // Show the bars
+    svg.append("g")
+      .selectAll("g")
+      // Enter in the stack data = loop key per key = group per group
+      .data(stackedData)
+      .enter().append("g")
+      .attr("fill", function (d) {
+        return color(d.key);
       })
-      .attr('x', function(d) {
-        return me.chartProps.x(d.pps) - 15 / 2;
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function (d) {
+        return d;
       })
-      .attr('width', Number(15))
-      .attr('y', function(d) {
-        return me.chartProps.y2(d.shareCount);
+      .enter().append("rect")
+      .attr("x", function (d) {
+        return x(d.data.pps);
       })
-      .attr('height', function(d) {
-        return height - me.chartProps.y2(d.shareCount);
-      });
+      .attr("y", function (d) {
+        return y(d[1]);
+      })
+      .attr("height", function (d) {
+        return y(d[0]) - y(d[1]);
+      })
+      .attr("width", x.bandwidth())
 
     // Add the valueline path.
     if (this.auction.reserveMet) {
       svg.append('line')
         .attr('class', 'line strikePriceLine')
-        .style('stroke', 'red')
+        .style('stroke', 'blue')
         .style('opacity', .8)
         .style('stroke-width', 4)
         .style('fill', 'none')
@@ -190,6 +214,43 @@ export class AuctionGraphComponent implements OnInit, OnChanges, AfterViewInit {
     }
   }
 
+  /**
+   * Method to parse the data into object readable by stacked bar
+   */
+  parseVolumeData() {
+    var tmpVolumeData = this.auction.volumeData
+    tmpVolumeData = this.removeDuplicates(tmpVolumeData, 'pps')
+    var shares = this.auction.sharesOffered
+    tmpVolumeData.sort((a, b) => (a.pps > b.pps) ? -1 : 1)
+    tmpVolumeData.forEach(function (bid: any) {
+      bid.winning = 0
+      bid.losing = 0
+      if (shares - bid.shareCount > 0) {
+        bid.winning = bid.shareCount == undefined ? 0 : bid.shareCount
+        bid.losing = 0
+        shares -= bid.shareCount
+      } else {
+        // The remaining losing bids for this bid (pps)
+        var bidLosing = bid.shareCount - shares
+        bid.winning = shares
+        bid.losing = bidLosing
+        shares = 0
+      }
+      delete bid.shareCount
+    })
+    tmpVolumeData['columns'] = ['pps', 'winning', 'losing']
+    return tmpVolumeData
+  }
+
+  /**
+   * Method to remove duplicate
+   * (used in buildChart to remove duplicate pps objects)
+   */
+  removeDuplicates(myArr, prop) {
+    return myArr.filter((obj, pos, arr) => {
+      return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
+  }
 
   updateChart() {
     d3.selectAll('svg').remove();
